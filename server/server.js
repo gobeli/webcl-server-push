08361f45f7eda0c92fb51935e249cbbe10b14f98
gobeli <etienne.gobeli@students.fhnw.ch>
2019-12-10 14:32:52 +0100

    fix server

--- server/server.js
+++ server/server.js
@@ -3,8 +3,9 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 
 const app = express();
-app.use(bodyParser.json());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.text());
 app.use(cors());
 
 let state = [];
@@ -13,9 +14,10 @@ app.get('/', function (req, res) {
   res.json(state);
 });
 
+
 app.post('/', function(req, res) {
-  console.log(req.body)
-  state = [...state, req.body];
+  const todo = JSON.parse(req.body);
+  state = [...state, todo];
   res.status(204).end();
 });
 
