3f40b6c54be06feaeb6308aae10bffa277edf89a
gobeli <etienne.gobeli@students.fhnw.ch>
2019-12-10 15:08:34 +0100

    adds debugging window

--- src/App.svelte
+++ src/App.svelte
@@ -2,12 +2,14 @@
 	import { onMount } from 'svelte';
 	import { writable } from 'svelte/store'
 	import TodoList from './TodoList.svelte';
+	import Debug from './Debug.svelte';
 	import LocalService from './services/local';
 	import PollingService from './services/polling';
 	import ServerSentEventsService from './services/sse';
 
 	const services = [LocalService, PollingService, ServerSentEventsService];
-	const todos = writable([]);
+	const todos$ = writable([]);
+	const service$ = writable(null);
 	let todoText, selectedService;
 
 	onMount(() => {
@@ -17,12 +19,11 @@
 	const selectService = serviceName  => {
 		if (selectedService) {
 			selectedService.disconnect();
-			console.log(`${selectedService.name} disconnected`);
 		}
 		const service = services.find(s => s.name === serviceName);
 		selectedService = service;
-		service.connect(todos);
-		console.log(`${service.name} connected`);
+		service.connect(todos$);
+		service$.set(service);
 	}
 
 	const submit = evt => {
@@ -32,14 +33,33 @@
 </script>
 
 <main>
-	<select name="services" on:change={evt => selectService(evt.target.value)}>
-		{#each services as service}
-			<option value={service.name}>{service.name}</option>
-		{/each}
-	</select>
-	<form on:submit={evt => submit(evt)}>
-		<input type="text" name="text" bind:value={todoText} placeholder="Todo">
-		<input type="submit" />
-	</form>
-	<TodoList todos$={todos}  />
+	<section>
+		<select name="services" on:change={evt => selectService(evt.target.value)}>
+			{#each services as service}
+				<option value={service.name}>{service.name}</option>
+			{/each}
+		</select>
+		<form on:submit={evt => submit(evt)}>
+			<input type="text" name="text" bind:value={todoText} placeholder="Todo">
+			<input type="submit" />
+		</form>
+		<TodoList todos$={todos$}  />
+	</section>
+	<section>
+		<Debug {todos$} {service$}  />
+	</section>
 </main>
+
+<style>
+	main {
+		display: grid;
+		max-width: 900px;
+		margin: auto;
+		grid-template-columns: 1fr 1fr;
+		grid-gap: 2rem;
+		height: 100%;
+	}
+	section {
+		height: 100%;
+	}
+</style>
---
+++ src/Debug.svelte
@@ -0,0 +1,31 @@
+<script>
+  import { onMount } from 'svelte';
+
+  let debugText = '';
+
+  onMount(() => {
+    todos$.subscribe(todos => {
+      debugText = `Received new state: \n${JSON.stringify(todos, undefined, 2)}\n\n${debugText}`;
+    });
+
+    service$.subscribe(service => {
+      if (service) {
+        debugText = `New service selected: ${service.name}`;
+      }
+    });
+  });
+
+  export let todos$, service$;
+</script>
+<pre>{debugText}</pre>
+<style>
+  pre {
+    max-height: 100vh;
+    height: 100%;
+    overflow: auto;
+    background-color: lightgray;
+    border: 2px solid #444;
+    padding: 1rem;
+    margin: 0;
+  }
+</style>
